8deb8943e4c689fe425e50047c4dcd648cf1983b
Matej Ramuta <matej.ramuta@gmail.com>
2013-12-13 13:30:05 +0000

    UrlsData.java edited online with Bitbucket

--- src/fashion/collage/app/data/UrlsData.java
+++ src/fashion/collage/app/data/UrlsData.java
@@ -1,7 +1,7 @@
 package fashion.collage.app.data;
 
 public class UrlsData {
-	private static final String PARTNER_ID = "uid1249-8197057-90";
+	private static final String PARTNER_ID = "secret-key";
 	
 	private static final String SHOPSTYLE_SHOES_IMAGES = "http://api.shopstyle.com/action/apiSearch?pid="+PARTNER_ID+"&cat=womens-shoes&min=0&count=12&format=json";
 	private static final String SHOPSTYLE_TOPS_IMAGES = "http://api.shopstyle.com/action/apiSearch?pid="+PARTNER_ID+"&cat=sweatshirts&min=0&count=12&format=json";
